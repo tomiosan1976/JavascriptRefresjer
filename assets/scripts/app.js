933907829fb7ffa0b5ca54b529169d3a2dd90ec5
Paulo <144171514+tomiosan1976@users.noreply.github.com>
2023-09-05 16:54:20 -0300

    Initial commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,7 @@
+//import { apiKey } from "./util";
+//import apiKey from "./util";
+//import { apiKey, abc } from "./util";
+
+import * as myimp from "./util";
+
+console.log(myimp.abc);
---
+++ assets/scripts/util.js
@@ -0,0 +1,3 @@
+export default "paulots";
+export let apiKey = "mykey123";
+export let abc = "abc_texto";
